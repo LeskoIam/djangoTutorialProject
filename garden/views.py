1aa4eb76b9c8318a4e4ef77f383917e269776bfd
Lesko <polensek.matevz@gmail.com>
2024-05-29 11:25:08 +0200

    Learned how to work with models.

--- garden/views.py
+++ garden/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import DetailView, ListView
+import logging
+
+from django.shortcuts import render
+from django.views.generic import ListView, TemplateView, View
 
 from .models import Garden, GardenBed, Plant
 
@@ -8,6 +11,13 @@ class GardenListView(ListView):
     template_name = "garden_list.html"
 
 
-class GardenDetailView(DetailView):
-    model = GardenBed
-    template_name = "garden_detail.html"
+class GardenDetailView(View):
+    # dartmouth = GardenBed.objects.get(garden_id=)
+    # dartmouth.student_set.all()  # returns all students at Dartmouth
+    # dartmouth.objects.filter(student__first_name='william')  # returns all Dartmouth students named William
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs["pk"]
+        garden = Garden.objects.get(pk=pk)
+        beds = GardenBed.objects.filter(garden=garden)
+        return render(request, "garden_detail.html", {"garden": garden, "beds": beds})
